fb4f08b064720dddc6a4ab8d7fffadda3f3d9d55
rkvadrat <romakvadrat@icloud.com>
2021-06-08 11:55:43 +0300

    camp2v

---
+++ js/script.js
@@ -0,0 +1,317 @@
+but.onclick = function () {
+    var elem = document.getElementById('fact1');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but").addEventListener("click", function(){
+    document.getElementById("f1").style.backgroundColor  = "#ff0000";
+});
+
+but1.onclick = function () {
+    var elem = document.getElementById('fact2');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but1").addEventListener("click", function(){
+    document.getElementById("f2").style.backgroundColor  = "#ff0000";
+});
+
+but2.onclick = function () {
+    var elem = document.getElementById('fact3');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but2").addEventListener("click", function(){
+    document.getElementById("f3").style.backgroundColor  = "#ff0000";
+});
+ // 
+but3.onclick = function () {
+    var elem = document.getElementById('fact4');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but3").addEventListener("click", function(){
+    document.getElementById("f4").style.backgroundColor  = "#ff0000";
+});
+
+//
+
+but4.onclick = function () {
+    var elem = document.getElementById('fact5');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but4").addEventListener("click", function(){
+    document.getElementById("f5").style.backgroundColor  = "#ff0000";
+});
+
+//
+
+but5.onclick = function () {
+    var elem = document.getElementById('fact6');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but5").addEventListener("click", function(){
+    document.getElementById("f6").style.backgroundColor  = "#ff0000";
+});
+
+// 
+
+but6.onclick = function () {
+    var elem = document.getElementById('fact7');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but6").addEventListener("click", function(){
+    document.getElementById("f7").style.backgroundColor  = "#ff0000";
+});
+
+// 
+
+but7.onclick = function () {
+    var elem = document.getElementById('fact8');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but7").addEventListener("click", function(){
+    document.getElementById("f8").style.backgroundColor  = "#ff0000";
+});
+
+// 
+
+but8.onclick = function () {
+    var elem = document.getElementById('fact9');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but8").addEventListener("click", function(){
+    document.getElementById("f9").style.backgroundColor  = "#ff0000";
+});
+
+// 
+
+but9.onclick = function () {
+    var elem = document.getElementById('fact10');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but9").addEventListener("click", function(){
+    document.getElementById("f10").style.backgroundColor  = "#ff0000";
+});
+
+// 
+
+but10.onclick = function () {
+    var elem = document.getElementById('fact11');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but10").addEventListener("click", function(){
+    document.getElementById("f11").style.backgroundColor  = "#ff0000";
+});
+
+// 
+
+but10.onclick = function () {
+    var elem = document.getElementById('fact11');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but10").addEventListener("click", function(){
+    document.getElementById("f11").style.backgroundColor  = "#ff0000";
+}); 
+
+// 
+
+but11.onclick = function () {
+    var elem = document.getElementById('fact12');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but11").addEventListener("click", function(){
+    document.getElementById("f12").style.backgroundColor  = "#ff0000";
+});
+
+// 
+
+but12.onclick = function () {
+    var elem = document.getElementById('fact13');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but12").addEventListener("click", function(){
+    document.getElementById("f13").style.backgroundColor  = "#ff0000";
+});
+
+// 
+
+but13.onclick = function () {
+    var elem = document.getElementById('fact14');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but13").addEventListener("click", function(){
+    document.getElementById("f14").style.backgroundColor  = "#ff0000";
+});
+
+// 
+
+but14.onclick = function () {
+    var elem = document.getElementById('fact15');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but14").addEventListener("click", function(){
+    document.getElementById("f15").style.backgroundColor  = "#ff0000";
+});
+
+// 
+but15.onclick = function () {
+    var elem = document.getElementById('fact16');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but15").addEventListener("click", function(){
+    document.getElementById("f16").style.backgroundColor  = "#ff0000";
+});
+
+// 
+but16.onclick = function () {
+    var elem = document.getElementById('fact17');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but16").addEventListener("click", function(){
+    document.getElementById("f17").style.backgroundColor  = "#ff0000";
+});
+
+// 
+but17.onclick = function () {
+    var elem = document.getElementById('fact18');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but17").addEventListener("click", function(){
+    document.getElementById("f18").style.backgroundColor  = "#ff0000";
+});
+
+// 
+but18.onclick = function () {
+    var elem = document.getElementById('fact19');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but18").addEventListener("click", function(){
+    document.getElementById("f19").style.backgroundColor  = "#ff0000";
+});
+
+// 
+but19.onclick = function () {
+    var elem = document.getElementById('fact20');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but19").addEventListener("click", function(){
+    document.getElementById("f20").style.backgroundColor  = "#ff0000";
+});
+
+// 
+but20.onclick = function () {
+    var elem = document.getElementById('fact21');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but20").addEventListener("click", function(){
+    document.getElementById("f21").style.backgroundColor  = "#ff0000";
+});
+
+// 
+but21.onclick = function () {
+    var elem = document.getElementById('fact22');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but21").addEventListener("click", function(){
+    document.getElementById("f22").style.backgroundColor  = "#ff0000";
+});
+
+// 
+but22.onclick = function () {
+    var elem = document.getElementById('fact23');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but22").addEventListener("click", function(){
+    document.getElementById("f23").style.backgroundColor  = "#ff0000";
+});
+
+// 
+but23.onclick = function () {
+    var elem = document.getElementById('fact24');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but23").addEventListener("click", function(){
+    document.getElementById("f24").style.backgroundColor  = "#ff0000";
+});
+
+// 
+but24.onclick = function () {
+    var elem = document.getElementById('fact25');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but24").addEventListener("click", function(){
+    document.getElementById("f25").style.backgroundColor  = "#ff0000";
+});
+
+// 
+but25.onclick = function () {
+    var elem = document.getElementById('fact26');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but25").addEventListener("click", function(){
+    document.getElementById("f26").style.backgroundColor  = "#ff0000";
+});
+
+// 
+but26.onclick = function () {
+    var elem = document.getElementById('fact27');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but26").addEventListener("click", function(){
+    document.getElementById("f27").style.backgroundColor  = "#ff0000";
+});
+
+// 
+but27.onclick = function () {
+    var elem = document.getElementById('fact28');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but27").addEventListener("click", function(){
+    document.getElementById("f28").style.backgroundColor  = "#ff0000";
+});
+
+// 
+but28.onclick = function () {
+    var elem = document.getElementById('fact29');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but28").addEventListener("click", function(){
+    document.getElementById("f29").style.backgroundColor  = "#ff0000";
+});
+
+// 
+but29.onclick = function () {
+    var elem = document.getElementById('fact30');
+    elem.innerHTML = "";
+}
+
+document.getElementById("but29").addEventListener("click", function(){
+    document.getElementById("f30").style.backgroundColor  = "#ff0000";
+});
